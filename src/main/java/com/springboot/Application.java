8cc1975582fdb501872cd7e5da38a88449edd1c5
dixit-sharma <dixit.sharma@globallogic.com>
2018-04-25 13:59:42 +0530

    revert

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -9,6 +9,5 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 public static void main(String[] args) {
 	SpringApplication.run(Application.class, args);
-	
 }
 }
